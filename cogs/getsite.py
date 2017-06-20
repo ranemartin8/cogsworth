14c9cdf830d4f88e8e9c3d77aeddb8b6fa53eb09
Rane Martin <ranemartin8@gmail.com>
2017-06-19 22:13:17 -0400

    added comments to .getsite.py

--- cogs/getsite.py
+++ cogs/getsite.py
@@ -9,38 +9,34 @@ import pprint
 
 
 class anothercog:
-    """This command translates Hooks JS synergies file into a readable json file"""
+    """This is a cog."""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(pass_context=True)
     async def syn(self,ctx):
-        """stuff"""
+        """This command translates Hooks JS synergies file into a readable json file"""
         url = 'https://raw.githubusercontent.com/hook/champions/master/src/data/synergies.js'
         async with aiohttp.get(url) as response:
-            hook = await response.text()# hlen = len(hook)
-            fm_str = '...fromId('
-            to_end = '].map((synergy'
-            find_start = hook.find(fm_str)
-            find_end = hook.find(to_end)
+            hook = await response.text()
+            find_start = hook.find('...fromId(') #finds first occurance of "...fromId"
+            find_end = hook.find('].map((synergy') #finds first occurance of "].map((synergy"
             hk_slice = hook[find_start:find_end].split('...fromId') #slice out all syns & split into blocks by "from" champion
-            c_all = {}
+            c_all = {} #define output dictionary
             for champblock in hk_slice:
-                syn_blk = champblock.split('...fromStars') #separate into lines by "to" champion
+                syn_blk = champblock.split('...fromStars') #separate into rows by "to" champion
                 i = 0
-                synrows = {}
-                ch_dict = {}
+                synrows = {} #define row dictionary
+                ch_dict = {} #define championblock dictionary
                 for champline in syn_blk:
-
-                    pattern_fromchamp = re.compile(r'(?<=CHAMPION\.)(\w+)')
-                    pattern_tochamp = re.compile(r'\(\d+,\s\d')
-
-                    if pattern_tochamp.match(champline):
-                        stars = re.search(r'(\d+),\s(\d+)',champline)
-                        effect = re.search(r'(?<=EFFECT\.)(\w+)',champline)
-                        ch_count = champline.count('CHAMPION')
-                        if ch_count > 1:
+                    pattern_fromchamp = re.compile(r'(?<=CHAMPION\.)(\w+)') #regex matching text immediately after "CHAMPION."
+                    pattern_tochamp = re.compile(r'\(\d+,\s\d') #regex matching "(#, #..."
+                    if pattern_tochamp.match(champline):  #if the line starts with "(#, #..." aka a 'toChamp' line
+                        stars = re.search(r'(\d+),\s(\d+)',champline) #find numbers (star values)
+                        effect = re.search(r'(?<=EFFECT\.)(\w+)',champline) #find effect values
+                        ch_count = champline.count('CHAMPION') #count # of "CHAMPION" occurances
+                        if ch_count > 1: #if there are more than 1 champions listed in this line, give each it's own line
                             cnt = 0
                             while cnt < ch_count:
                                 champname = re.findall(r'(?<=CHAMPION\.)\w+',champline)
@@ -51,27 +47,23 @@ class anothercog:
                         else:
                             champname = re.search(r'(?<=CHAMPION\.)(\w+)',champline)
                             tochamp = [champname.group(0),effect.group(0),stars.group(1),stars.group(2)]
-                            synrows.update({"{}".format(i) : tochamp})
+                            synrows.update({"{}".format(i) : tochamp}) #set contains of synergy row
                             i += 1
-                    elif pattern_fromchamp.search(champline):
+                    elif pattern_fromchamp.search(champline): #if the line contains "CHAMPION.#####"
                         frmchamp = champline.strip()
                         fchamp = re.search(r'(?<=CHAMPION\.)(\w+)',frmchamp)
                         fromchamp = fchamp.group(0)
-                        ch_dict.update({fromchamp : [synrows]})
-                c_all.update(ch_dict)
-
-
+                        ch_dict.update({fromchamp : [synrows]}) #set contents of champion block with champ name and syn rows
+                c_all.update(ch_dict) #append all champion blocks to final output dictionary
             if find_start > 0:
                 try:
                     text_file = open("Synergies.json", "w")
-                    str_all = str(c_all).replace('\'',"\"")
-                #    pretty_call = pprint(str_all)
-                    text_file.write(str_all)
-                #    text_file.write(pretty_call.replace('\'',"\""))
+                    str_all = str(c_all).replace('\'',"\"") #convert to string. alnd json doesn't like single quotes
+                    text_file.write(str_all) #overwrites existing content
                     text_file.close()
                     await self.bot.send_file(ctx.message.channel,"Synergies.json")
                 except:
-                    raise
+                    raise #idk...
             else:
                 await self.bot.say("nada")
 
